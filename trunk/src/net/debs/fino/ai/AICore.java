9ef1c7aa34f8a308d41407d4844ac893825b56f1
agrischenko@gmail.com <agrischenko@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-09-07 13:01:21 +0000

    добавление AI core скриптования

---
+++ trunk/src/net/debs/fino/ai/AICore.java
@@ -0,0 +1,20 @@
+package net.debs.fino.ai;
+
+import net.debs.fino.Action;
+import net.debs.fino.DebsMap;
+import net.debs.fino.GameObject;
+
+/**
+ * Реализация пользовательского скрипта
+ * используется LUA
+ * @author AGrischenko
+ *
+ */
+public class AICore {
+
+	public Action getAction(GameObject gameObject, DebsMap map) {
+		Action action = null;
+		return action;
+	}
+
+}
