948b99f1455d17d03da5625d4e0277803633d943
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-10-27 13:43:13 +0000

    ДОбавленны геттеры и сеттеры

--- trunk/JDEBS/src/net/debs/fino/Action.java
+++ trunk/JDEBS/src/net/debs/fino/Action.java
@@ -1,6 +1,21 @@
 package net.debs.fino;
 
 public class Action {
+	
 	protected String type;
 	protected long id;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
 }
