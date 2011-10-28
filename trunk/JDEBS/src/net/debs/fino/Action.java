138a3bd5d6d52f83e90cfbc6de2f504bab179781
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-10-28 13:00:51 +0000

    Добавлена возможность указывать point (необходимо для действия move, чтобы указать в какую точку двигатся)

--- trunk/JDEBS/src/net/debs/fino/Action.java
+++ trunk/JDEBS/src/net/debs/fino/Action.java
@@ -2,8 +2,12 @@ package net.debs.fino;
 
 public class Action {
 	
+	public static final String Move = "move";
+	public static final String Attack = "attack";
+	
 	protected String type;
-	protected long id;
+	protected String id;
+	protected MapPoint point;
 	
 	public String getType() {
 		return type;
@@ -11,11 +15,17 @@ public class Action {
 	public void setType(String type) {
 		this.type = type;
 	}
-	public long getId() {
+	public String getId() {
 		return id;
 	}
-	public void setId(long id) {
+	public void setId(String id) {
 		this.id = id;
+	}
+	public MapPoint getPoint() {
+		return point;
+	}
+	public void setPoint(MapPoint point) {
+		this.point = point;
 	}
 	
 }
