45e46d70a169d2bdfb005697d5e7e130f22a249f
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-10-28 11:52:27 +0000

    Добавлены класы описывающие союзного и вражеского юнитов

---
+++ trunk/JDEBS/src/net/debs/fino/ai/AIAlly.java
@@ -0,0 +1,15 @@
+package net.debs.fino.ai;
+
+import net.debs.fino.GameObject;
+
+/**
+ * Реализация класа описывающего союзного юнита
+ * @author AAntonenko
+ */
+public class AIAlly extends AIGameObject {
+
+	public AIAlly(GameObject object) {
+		super(object);
+	}
+
+}
---
+++ trunk/JDEBS/src/net/debs/fino/ai/AIEnemy.java
@@ -0,0 +1,15 @@
+package net.debs.fino.ai;
+
+import net.debs.fino.GameObject;
+
+/**
+ * Реализация класа описывающего юнита противника (или м.б. нетрального)
+ * @author AAntonenko
+ */
+public class AIEnemy extends AIGameObject {
+
+	public AIEnemy(GameObject object) {
+		super(object);
+	}
+
+}
