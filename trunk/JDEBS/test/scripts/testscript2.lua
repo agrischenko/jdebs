9542c8da0a72cc1ba85b69e7e8ce76d5d1bbfdb9
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-11-04 08:28:16 +0000

    Убраны Deprecated методы, отлажена скриптовая функция getNearestEnemy

--- trunk/JDEBS/test/scripts/testscript2.lua
+++ trunk/JDEBS/test/scripts/testscript2.lua
@@ -1,7 +1,31 @@
-x = me:getMapPoint():getX();
-y = me:getMapPoint():getY();
+meX = me:getMapPoint():getX();
+meY = me:getMapPoint():getY();
 
-x = x + 1
+enemy = me:getNearestEnemy();
+if (enemy == nil) then
+	return
+end 
+enemyMapPoint = enemy:getMapPoint();
+
+enemyX = enemyMapPoint:getX();
+enemyY = enemyMapPoint:getY();
+
+print("enemy ("..enemyX..":"..enemyY..")");
+
+x = enemyX;
+y = enemyY;
+
+if meX < enemyX then
+	x = x - 1;
+elseif meX > enemyX then
+	x = x + 1;
+end
+
+if meY < enemyY then
+	y = y - 1;
+elseif meY > enemyY then
+	y = y + 1;
+end
 
 action:setType("move");
 action:setMapPoint(MapPoint(x, y));
