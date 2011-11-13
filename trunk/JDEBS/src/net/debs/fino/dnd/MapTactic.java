cdcda2262b79dfc0e7f6071f1951be58afff4d42
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-11-13 19:53:26 +0000

    Добавлена реализация поиска ближайшей для атаки клетки

---
+++ trunk/JDEBS/src/net/debs/fino/dnd/MapTactic.java
@@ -0,0 +1,66 @@
+package net.debs.fino.dnd;
+
+import java.util.Vector;
+
+import net.debs.fino.DebsMap;
+import net.debs.fino.MapPoint;
+
+/**
+ * Класс определения ближайшей клетки с которой можно атаковать
+ * @author AAntonenko
+ */
+public class MapTactic {
+
+	//Тест поиска
+	public static void main(String [] args){
+			
+		DebsMap map = new DebsMap();
+		map.setWidth(30);
+		map.setHeight(30);
+		
+		MapPoint res = getNearestAttackMapPoint(map, new MapPoint(0, 0), new MapPoint(7, 5), 4, 3);
+		
+		System.out.println(res);
+		
+	}
+	
+	/**
+	 * Возвращает ближайшую клетку для клетки <code>objectPoint</code> путь до которой не длинее чем <code>speed</code>, с которой можно видеть клетку <code>attackPoint</code> и расстояние до нее не больше <code>distance</code> 
+	 * @param map карта на которой будт расчет
+	 * @param objectPoint клетка от которой исчется необходимая клетка
+	 * @param attackPoint целевая клетка
+	 * @param speed максимальное расстояние от исходной клетки
+	 * @param distance расстояние до клетки цели
+	 * @return искомая клетка
+	 */
+	public static MapPoint getNearestAttackMapPoint(DebsMap map, MapPoint objectPoint, MapPoint attackPoint, int speed, int distance){
+		
+		MapPoint res = null;
+		int resDistance = -1;
+		
+		// Если искомой клеткой является текущая (хватает расстояние и целевая клетка видна)
+		if (MapDistance.distance(map, objectPoint, attackPoint) <= distance){
+			if (MapVisibility.see(map, objectPoint, attackPoint)) return new MapPoint(objectPoint.getX(), objectPoint.getY());
+		}
+		// Если расстояние сишком большое
+		else if ((MapDistance.distance(map, objectPoint, attackPoint) - 1) > (speed + distance)) return null;
+		
+		for (int radius = 1; radius <= speed; radius++) {
+			//Клетки находящиеся в радиусе radius от начальной
+			Vector<MapPoint> points = MapMask.getMapPointsByDistance(map, objectPoint, radius);
+			
+			for (MapPoint point : points) {
+				if (MapDistance.distance(map, point, attackPoint) <= distance) {
+					int dist = MapPath.path(map, objectPoint, point).distance();
+					if (resDistance < dist) {
+					res = point;
+					resDistance = dist;
+					}
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+}
