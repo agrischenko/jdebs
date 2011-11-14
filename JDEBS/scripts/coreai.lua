8223a4deb68620aa7f71964325edd5b0eb8224fd
aantonenko85@gmail.com <aantonenko85@gmail.com@4bac2f70-f0cf-4ee3-9812-ffc1b3bf5bb2>
2011-11-14 13:31:13 +0000

    Добавлена возможность вызова функций Random в скрипте реализованы на Java

---
+++ JDEBS/scripts/coreai.lua
@@ -0,0 +1,11 @@
+function Random()
+	return LuaRandom:random();
+end
+
+function Random(rand)
+	return LuaRandom:random(rand);
+end
+
+function Random(rand1, rand2)
+	return LuaRandom:random(rand1, rand2);
+end
